35bcd6a1a62d0678391badc7e0cde949964f645c
Cowelist <Henry8bombomdog@gmail.com>
2025-03-06 22:40:42 +0100

    Script: basic_player_mechanics.gd
    Har lagd en base karakter movment og animasjoner for bevegelsene. Jeg fikk så hvit starta med å lage flip Animasjoner for spiller bevegelsen.
    
    Jeg har også lagdt på en ny placeholder pakke siden den andre ikke hadde en opp og ned bevegelse animasjon.

---
+++ Script/Character/Main_player/basic_player_mechanics.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const speed = 100 
+
+func _physics_process(delta): 
+	var move_direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("dir_Right"):
+		move_direction.x += 1
+	elif Input.is_action_pressed("dir_Left"):
+		move_direction.x -= 1
+	if Input.is_action_pressed("dir_Down"):
+		move_direction.y += 1
+	elif Input.is_action_pressed("dir_Up"):
+		move_direction.y -= 1
+	
+	velocity = Vector2(move_direction * speed)
+	move_and_slide()
+	
