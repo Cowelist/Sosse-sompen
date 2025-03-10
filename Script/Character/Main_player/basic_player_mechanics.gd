314d020a7714565f8cd35b1a6c499d900d9757c8
Cowelist <Henry8bombomdog@gmail.com>
2025-03-10 15:10:04 +0100

    Akkselerasjon
    Ayas hjalp meg

--- Script/Character/Main_player/basic_player_mechanics.gd
+++ Script/Character/Main_player/basic_player_mechanics.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var speed = 100
 const max_speed = 100
 var weight = 60
+var accumelation = Vector2(1, 1)
 
 var current_direction = "none"
 
@@ -16,18 +17,38 @@ func _physics_process(delta):
 	if Input.is_action_pressed("dir_Right"):
 		current_direction = "right"
 		move_direction.x = 1
-		#speed_handler(1)
+		
 	if Input.is_action_pressed("dir_Down"):
 		current_direction = "down"
 		move_direction.y += 1
+	
+	
 	if Input.is_action_pressed("dir_Up"):
 		current_direction = "up"
 		move_direction.y -= 1
+
 	if Input.is_action_pressed("dir_Left"):
 		current_direction = "left"
 		move_direction.x -= 1
+		
+	print(accumelation)
+	
+	if move_direction.x != 0:
+		accumelation.x += 0.05
+		if accumelation.x > 3:
+			accumelation.x = 3
+	else:
+		accumelation.x = 0
 
-	velocity = Vector2(move_direction * speed)
+	
+	if move_direction.y != 0:
+		accumelation.y += 0.05
+		if accumelation.y > 3:
+			accumelation.y = 3
+	else:
+		accumelation.y = 0
+
+	velocity = Vector2(move_direction * speed * accumelation)
 	play_anim(move_direction)
 	move_and_slide()
 	
