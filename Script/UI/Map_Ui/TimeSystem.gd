9483b485744198e6e789bbac06a4a7defb735738
elev <Daniel.nordhaug@outlook.com>
2025-03-14 13:37:06 +0100

    LA INN TIMESYSTEM

--- Script/UI/Map_Ui/TimeSystem.gd
+++ Script/UI/Map_Ui/TimeSystem.gd
@@ -1,3 +1,9 @@
 class_name TimeSystem extends Node
 
-@export var date_time: DateTime 
+@export var date_time: DateTime
+@export var ticks_pr_sec: int = 6
+
+func _process(delta: float) -> void:
+	date_time.increas_by_sec(delta * ticks_pr_sec)
+	
+	
