1df906854dbabfe173154496a30f527c1189f1f0
Cowelist <Henry8bombomdog@gmail.com>
2025-03-17 15:09:30 +0100

    interaction zone
    Har ikke kommet lagt, men fiksa det sånn at interaction sonen fungerer siden den ikke gjorde det tidligere. Endret på feil mask layer

--- Script/Character/area_fishing_zone.gd
+++ Script/Character/area_fishing_zone.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends Area2D
+#class_name Fishing_interaction_area
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func _on_body_entered(body: Node2D) -> void:
+	print("hei")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_entered(area: Area2D) -> void:
+	print("player on area enterd")
