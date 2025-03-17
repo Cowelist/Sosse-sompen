71f6bd390e850546a497cbbc9277783074ba4b76
elev <Daniel.nordhaug@outlook.com>
2025-03-17 11:01:39 +0100

    nesten ferdig med time greia mi

--- Script/UI/Map_Ui/Date_Time.gd
+++ Script/UI/Map_Ui/Date_Time.gd
@@ -1,9 +1,30 @@
 class_name DateTime extends Resource
 
+@export_range(0,59) var Seconds: int = 0
 @export_range(0,59) var Minutes: int = 0 
 @export_range(0,23) var Hours: int = 0
 @export_range(0,10) var Days: int = 0
 
-func increas_by_sec(delta_Minutes: float) -> void:
-	pass
+var delta_time: float = 0
+
+func increas_by_sec(delta_Seconds: float) -> void:
+	delta_time += delta_Seconds
+	if delta_time < 1: return
+	
+	var delta_int_secs: int = delta_time
+	delta_time -= delta_int_secs
+	
+	Seconds += delta_int_secs
+	Minutes += Seconds/ 60
+	Hours += Minutes/ 60
+	Days += Hours/ 24
+	
+	Seconds = Seconds % 60
+	Minutes = Minutes % 60
+	Hours = Hours % 24
+	
+	print_debug(str(Days) + ":" + str(Hours) + ":" + str(Minutes) + ":" + str(Seconds))
+	 
+	
+	
 	
