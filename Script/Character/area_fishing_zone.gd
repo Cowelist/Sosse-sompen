de3832ac6408f1ebf6c65c979ef8f5fa76f51e3e
Cowelist <Henry8bombomdog@gmail.com>
2025-03-18 20:59:50 +0100

    Signal (fishing)
    Har ikke gjort mye her. Har bare for det meste lært litt om signaler i godot. ikke mye

--- Script/Character/area_fishing_zone.gd
+++ Script/Character/area_fishing_zone.gd
@@ -1,11 +1,11 @@
 extends Area2D
 #class_name Fishing_interaction_area
 
-
+func _ready() -> void:
+	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
+	player_inventory.updated_inventory.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
+	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
+	print(player_inventory.inventory_space)
 	print("hei")
-
-
-func _on_area_entered(area: Area2D) -> void:
-	print("player on area enterd")
