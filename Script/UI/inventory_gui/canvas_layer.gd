97be038e04dc44fbf00d405e8a778b90175b1d74
Dayluf <Daniel.nordhaug@outlook.com>
2025-03-19 22:35:33 +0100

    how, har noen av disse filene på gitignore men tydeligvis

--- Script/UI/inventory_gui/canvas_layer.gd
+++ Script/UI/inventory_gui/canvas_layer.gd
@@ -1,11 +1,13 @@
 extends CanvasLayer
 
+@onready var inventory = $Inventory_gui
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	inventory.close()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_inventory"):
+		if inventory.isOpen:
+			inventory.close()
+		else:
+			inventory.open()
