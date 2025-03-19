4482ad6153c9fb061178e7f256f5bfd70a5207f7
elev <Daniel.nordhaug@outlook.com>
2025-03-19 10:32:48 +0100

    ok

--- Script/Character/area_fishing_zone.gd
+++ Script/Character/area_fishing_zone.gd
@@ -1,11 +1,11 @@
 extends Area2D
 #class_name Fishing_interaction_area
 
-func _ready() -> void:
-	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
-	player_inventory.updated_inventory.connect(_on_body_entered)
+#func _ready() -> void:
+#	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
+#	player_inventory.updated_inventory.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
-	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
-	print(player_inventory.inventory_space)
+#	var player_inventory = get_node("res://Script/Character/Main_player/Inventroy.gd")
+#	print(player_inventory.inventory_space)
 	print("hei")
