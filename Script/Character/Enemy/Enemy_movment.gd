500a36af03dc66d6bbc3053085bfac292e515146
Cowelist <Henry8bombomdog@gmail.com>
2025-03-20 12:51:15 +0100

    Ai_movment
    Ai movment er good, men må fortsatt styre hvor den går

--- Script/Character/Enemy/Enemy_movment.gd
+++ Script/Character/Enemy/Enemy_movment.gd
@@ -3,62 +3,56 @@ extends CharacterBody2D
 @onready var animation_sprite = $Enemy_movment
 
 var current_direction = "none"
-var direction = velocity.normalized()
 
 func _physics_process(delta: float) -> void:
-	var dir = direction
-	if dir.x > 0:
+	var dir = velocity
+	if velocity.x > 0:
 		current_direction = "right"
-		print("Det her right")
 	elif dir.x < 0:
 		current_direction = "left"
-		print("Det her left")
 	if dir.y > 0:
 		current_direction = "down"
-		print("Det her down")
 	elif dir.y < 0:
 		current_direction = "up"
-		print("Det her up")
-		
 	
-	play_anim()
+	print("Det her vel", + dir)
+	play_anim(dir)
 	move_and_slide()
 
+func movment_x(dir_x, speed):
+	velocity.x = dir_x * speed
+	print("Det her dir _x", + velocity)
+func movment_y(dir_y, speed):
+	velocity.y = dir_y * speed
+	print("Det her dir _y", + velocity)
 
-func movment(dir, speed):
-	velocity.x = dir * speed
-	velocity.y = dir * speed
 
-
-
-func play_anim():
+func play_anim(move_dir):
 	var dir = current_direction
-	print("Det her", + direction)
-
 	if dir == "right":
-		animation_sprite.flip_h = true
-		if direction.x > 0: 
+		animation_sprite.flip_h = false
+		if move_dir.x != 0: 
 			animation_sprite.play("walk_side")
 		else:
 			animation_sprite.play("idle_side")
 
 	if dir == "left":
-		animation_sprite.flip_h = false 
-		if direction.x < 0:
+		animation_sprite.flip_h = true
+		if move_dir.x != 0:
 			animation_sprite.play("walk_side")
 		else:
 			animation_sprite.play("idle_side")
 			
 	if dir == "up":
 		animation_sprite.flip_h = true
-		if direction.y > 0:
+		if move_dir.y != 0:
 			animation_sprite.play("walk_back")
 		else:
 			animation_sprite.play("idle_back")
 
 	if dir == "down":
 		animation_sprite.flip_h = true
-		if direction.y < 0:
+		if move_dir.y != 0:
 			animation_sprite.play("walk_front")
 		else:
 			animation_sprite.play("idle_front")
--- ai/tasks/choose_random_position.gd
+++ ai/tasks/choose_random_position.gd
@@ -5,8 +5,8 @@ extends BTAction
 
 #& kan bli sett på som en global variable | kommer til å bli kalt på gjennom et annet script 
 @export var position_var: StringName = &"pos"
-@export var dir_var: StringName = &"dir"
-var tr = false 
+@export var dir_var_x: StringName = &"dir_x"
+@export var dir_var_y: StringName = &"dir_y"
 
 #Func som kjører til den får en spesifikk (SUCCESS) retur verdi
 func _tick(_delta: float) -> Status:
@@ -17,18 +17,18 @@ func _tick(_delta: float) -> Status:
 #Gir postion_var den samme verdien som pos/random_position funksjonen
 	blackboard.set_var(position_var, pos)
 	
-	print("Klag", + dir, pos)
 	return SUCCESS
 	
 	
 #Gir en tilfeldig posisjon som karakteren skal bevege seg til 
 func random_position(dir):
 	var vector: Vector2
-	var distance = randi_range(range_min_dir, range_max_dir) * dir 
+	var distance_x = randi_range(range_min_dir, range_max_dir) * dir 
+	var distance_y = randi_range(range_min_dir, range_max_dir) * dir
 
 #Lager en tilfeldig 
-	var final_position_x = (distance + agent.global_position.x)
-	var final_position_y = (distance + agent.global_position.y)
+	var final_position_x = (distance_x + agent.global_position.x)
+	var final_position_y = (distance_y + agent.global_position.y)
 	
 	vector.x = final_position_x
 	vector.y = final_position_y
@@ -36,12 +36,24 @@ func random_position(dir):
 
 #Gir en tilfeldig rettning som karakteren skal bevege seg mot	
 func random_dir():
-	var dir = randi_range(-4, 3)
-	if abs(dir) != dir:
-		dir = -1
+	var dir_x = randi_range(-2, 3)
+	var dir_y = randi_range(-2, 3)
+	if abs(dir_x) != dir_x:
+		dir_x = -1
+	elif dir_x <= 2:
+		dir_x = 0
+	else: 
+		dir_x = 1
+		
+	if abs(dir_y) != dir_y:
+		dir_y = -1
+	elif dir_y <= 2:
+		dir_y = 0
 	else:
-		dir = 1
+		dir_y = 1
 #Blackboard = Er som en variabel som ai-en kan gå gjennom og endre på under kjøringen av spillet
 #Den kan f.eks. se etter en spiller og deretter oppdatere bb sånn at en annen funksjon eller noe kan få nytte av informasjonen
-	blackboard.set_var(dir_var, dir)
-	return dir 
+	blackboard.set_var(dir_var_x, dir_x)
+	blackboard.set_var(dir_var_y, dir_y)
+	return dir_x
+	return dir_y 
