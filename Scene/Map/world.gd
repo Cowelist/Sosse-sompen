025f9c3165ab6c44788866298ffb9f7a6359b9cb
elev <Daniel.nordhaug@outlook.com>
2025-03-24 12:59:24 +0100

    mer kommentarer

--- Scene/Map/world.gd
+++ Scene/Map/world.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+#funtion for når inventory er lukket
 func _on_inventory_gui_closed() -> void:
 	get_tree().paused = false
 
-
+#function for når inventory er åpnet, pauser alt annet i spillet
 func _on_inventory_gui_opened() -> void:
 	get_tree().paused = true
